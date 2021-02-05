ec374dee832ae4fae609a7c9c282b9d8c1136b43
Your Name <you@example.com>
2021-02-05 18:40:42 +0530

    firstpull

---
+++ src/org/test/B.java
@@ -0,0 +1,13 @@
+
+	package org.test;
+
+	public class B {
+		public B() {
+			System.out.println(" Par Default constructor");
+		}
+
+		  public B(int id) {
+		    System.out.println(id);
+		  }
+
+		}
