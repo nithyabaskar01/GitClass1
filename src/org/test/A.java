f0e4dd999a5b5d03bb1b46cd799ce01dc63cb74d
Your Name <you@example.com>
2021-03-13 11:35:47 +0530

    Shankar

--- src/org/test/A.java
+++ src/org/test/A.java
@@ -4,10 +4,14 @@ package org.test;
 		public A() {
           super(12);
 			    System.out.println("Default const...");
-			  }
+		System.out.println("Nithyaaa");	 
+		}
 
 			  public static void main(String[] args) {
 			    A a = new A();
+			  
+			  
 			  }
+			  
 
 }
